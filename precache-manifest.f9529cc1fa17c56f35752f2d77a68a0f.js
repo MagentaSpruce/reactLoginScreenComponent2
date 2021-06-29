280894e87a91d40709d47c8ab9d824b1b3dec749
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-06-29 11:08:07 -0400

    Updates

---
+++ precache-manifest.f9529cc1fa17c56f35752f2d77a68a0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95bbb4ce06a0cf97fce03ea6f5b778e8",
+    "url": "/reactLoginScreenComponent2/index.html"
+  },
+  {
+    "revision": "1019d90815a170539af5",
+    "url": "/reactLoginScreenComponent2/static/js/2.f6b5b966.chunk.js"
+  },
+  {
+    "revision": "e8bbdf0e79c6f89d4716",
+    "url": "/reactLoginScreenComponent2/static/js/main.58b3abe1.chunk.js"
+  },
+  {
+    "revision": "ec1fd35f542d9590e6ac",
+    "url": "/reactLoginScreenComponent2/static/js/runtime-main.da2d58ea.js"
+  }
+]);
